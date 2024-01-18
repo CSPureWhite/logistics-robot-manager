c535ae2be8ab7d3026b715d14f5a14ddd4160935
CSPureWhite <1461459810@qq.com>
2024-01-18 22:24:16 +0800

    项目运行所需的sql文件（目前包含user表）

--- src/main/resources/logistics_robot_manager.sql
+++ src/main/resources/logistics_robot_manager.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80031
  Source Host           : localhost:3306
- Source Schema         : logistics_robor_manager
+ Source Schema         : logistics_robot_manager
 
  Target Server Type    : MySQL
  Target Server Version : 80031
