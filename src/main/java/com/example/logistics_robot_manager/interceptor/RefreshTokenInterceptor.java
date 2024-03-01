c246d6a11a5cf88a7c0499d84c5c838fa9f05b56
CSPureWhite <1461459810@qq.com>
2024-03-01 17:50:56 +0800

    添加token自动续期功能

---
+++ src/main/java/com/example/logistics_robot_manager/interceptor/RefreshTokenInterceptor.java
@@ -0,0 +1,39 @@
+package com.example.logistics_robot_manager.interceptor;
+
+import com.example.logistics_robot_manager.common.Constant;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 访问所有页面都会自动将token续期
+ */
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+    StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate=stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token=request.getHeader("authorization");
+        if(StringUtils.isBlank(token)){
+            // token不存在，放行
+            return true;
+        }
+        String tokenKey=Constant.LOGIN_USER_KEY+token;
+        String userId =stringRedisTemplate.opsForValue().get(tokenKey);
+        if(StringUtils.isBlank(userId)){
+            // 用户不存在，放行
+            return true;
+        }
+        // 刷新token有效期
+        stringRedisTemplate.expire(tokenKey, Constant.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        return true;
+    }
+}
