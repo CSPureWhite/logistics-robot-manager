a0fbb48c329e547ea386b22022912ddd321e2aa0
CSPureWhite <1461459810@qq.com>
2024-03-02 17:12:20 +0800

    添加用户登录权限校验功能

--- src/main/java/com/example/logistics_robot_manager/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/logistics_robot_manager/interceptor/LoginInterceptor.java
@@ -1,11 +1,14 @@
 package com.example.logistics_robot_manager.interceptor;
 
+import com.example.logistics_robot_manager.common.Constant;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 
 public class LoginInterceptor implements HandlerInterceptor {
 
@@ -18,8 +21,14 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token=request.getHeader("authorization");
-        if(StringUtils.isBlank(token)||StringUtils.isBlank(stringRedisTemplate.opsForValue().get(token))){
-            // token为空或用户不存在，拒绝放行，并将状态码设为401
+        if(StringUtils.isBlank(token)){
+            // token为空，拒绝放行，并将状态码设为401
+            response.setStatus(401);
+            return false;
+        }
+        Map<Object, Object> userMap=stringRedisTemplate.opsForHash().entries((Constant.LOGIN_USER_KEY+token));
+        if(userMap.isEmpty()){
+            // 用户不存在，拒绝放行，并将状态码设为401
             response.setStatus(401);
             return false;
         }
