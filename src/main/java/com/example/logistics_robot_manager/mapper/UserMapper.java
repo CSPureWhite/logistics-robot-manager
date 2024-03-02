bd1454a3ac3248794c3beabc7c80dedc804ce907
CSPureWhite <1461459810@qq.com>
2024-03-02 16:01:51 +0800

    常量、注解、配置文件等杂项修改

--- src/main/java/com/example/logistics_robot_manager/mapper/UserMapper.java
+++ src/main/java/com/example/logistics_robot_manager/mapper/UserMapper.java
@@ -2,8 +2,6 @@ package com.example.logistics_robot_manager.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.logistics_robot_manager.entity.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserMapper extends BaseMapper<User> {
 }
