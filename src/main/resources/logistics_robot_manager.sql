0243019a1ab6a2d4b8ac570a361834f7957227f8
CSPureWhite <1461459810@qq.com>
2024-03-04 14:58:13 +0800

    数据库建表文件

--- src/main/resources/logistics_robot_manager.sql
+++ src/main/resources/logistics_robot_manager.sql
@@ -11,11 +11,49 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 16/01/2024 15:00:03
+ Date: 04/03/2024 14:57:12
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `goods_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '货物id',
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '货物名称',
+  `goods_status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '货物状态标识，0为未运输，1为运输中，2为已在架',
+  `goods_type` bigint NULL DEFAULT NULL COMMENT '货物类型',
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '货物的生产时间',
+  `shelving_time` timestamp(0) NULL DEFAULT NULL COMMENT '货物的上架时间',
+  `details` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '货物明细',
+  `shelf_id` bigint UNSIGNED NULL DEFAULT NULL COMMENT '所在货架的id',
+  PRIMARY KEY (`goods_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for shelf
+-- ----------------------------
+DROP TABLE IF EXISTS `shelf`;
+CREATE TABLE `shelf`  (
+  `shelf_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '货架id',
+  `shelf_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '货架名称',
+  `shelf_status` tinyint(1) NOT NULL COMMENT '货架状态，0为无货物，1为有货物，2为已满',
+  `shelf_type` int NULL DEFAULT NULL COMMENT '货架类型，1为samll，2为middle，3为large',
+  `goods_amount` int NULL DEFAULT NULL COMMENT '货架存放的货物数量',
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '激活时间',
+  PRIMARY KEY (`shelf_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of shelf
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
@@ -31,7 +69,7 @@ CREATE TABLE `user`  (
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最近一次更新时间',
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
