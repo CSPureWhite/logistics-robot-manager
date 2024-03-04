98d08eefe293a01178310aab0cc6646f8a4a32fc
CSPureWhite <1461459810@qq.com>
2024-03-04 14:59:51 +0800

    redis缓存的用户信息实体

---
+++ src/main/java/com/example/logistics_robot_manager/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.example.logistics_robot_manager.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long userId;
+    private String userName;
+    private Integer userType;
+}
