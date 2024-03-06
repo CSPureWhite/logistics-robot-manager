4493b4c92c45b6a0cc1660bad2b4459c4dd5c437
CSPureWhite <1461459810@qq.com>
2024-03-06 16:15:49 +0800

    货物管理、货架管理列表按照记录的创建时间倒序排列

--- src/main/java/com/example/logistics_robot_manager/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/example/logistics_robot_manager/service/impl/GoodsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.logistics_robot_manager.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.logistics_robot_manager.dto.GoodsTypeCountDTO;
@@ -18,7 +19,7 @@ public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements
      */
     @Override
     public Page<Goods> queryAll(Integer currentPage, Integer pageSize) {
-        return page(new Page<>(currentPage,pageSize));
+        return page(new Page<>(currentPage,pageSize)).addOrder(OrderItem.desc("create_time")); // 按照生产时间倒序排列
     }
 
     /**
@@ -27,7 +28,8 @@ public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements
     @Override
     public Page<Goods> queryByKey(Integer currentPage, Integer pageSize, String key) {
         return page(new Page<>(currentPage,pageSize),
-                query().eq("goods_id",key).or().like("goods_name","%"+key+"%"));
+                query().eq("goods_id",key).or().like("goods_name","%"+key+"%"))
+                .addOrder(OrderItem.desc("create_time")); // 按照生产时间倒序排列
     }
 
     /**
--- src/main/java/com/example/logistics_robot_manager/service/impl/ShelfServiceImpl.java
+++ src/main/java/com/example/logistics_robot_manager/service/impl/ShelfServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.logistics_robot_manager.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.logistics_robot_manager.entity.Shelf;
@@ -11,12 +12,13 @@ import org.springframework.stereotype.Service;
 public class ShelfServiceImpl extends ServiceImpl<ShelfMapper, Shelf> implements IShelfService {
     @Override
     public Page<Shelf> queryAll(Integer currentPage, Integer pageSize) {
-        return page(new Page<>(currentPage,pageSize));
+        return page(new Page<>(currentPage,pageSize)).addOrder(OrderItem.desc("create_time")); // 按激活时间倒序排列
     }
 
     @Override
     public Page<Shelf> queryByKey(Integer currentPage, Integer pageSize, String key) {
         return page(new Page<>(currentPage, pageSize),
-                query().eq("goods_id", key).or().like("goods_name", "%" + key + "%"));
+                query().eq("goods_id", key).or().like("goods_name", "%" + key + "%"))
+                .addOrder(OrderItem.desc("create_time")); // 按照激活时间倒序排列
     }
 }
